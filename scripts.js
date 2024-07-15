2226b8b81433d053d1f81dac365e688b803c0483
MariuszNi <140714588+MariuszNi@users.noreply.github.com>
2024-07-15 10:45:26 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,37 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = 'AIzaSyBLRzMMAv3iAZWUe4y346bmDOSyYymRR3Y';
+    const channelIds = [
+        'UC8R84d88YJDXLHRwnTrbNbw',  // Polski
+        'UCZbU79MK6RxCKSQWfjIj1oQ',  // Turecko/Angielski
+        'UC2aryP954fHFb9vld3Hk3OA',  // Angielski
+        'UCKRvfdO9-HaR9ix4a-0LrOw'   // Angielski
+    ];
+
+    const videoColumns = [
+        document.getElementById('video-list-1'),
+        document.getElementById('video-list-2'),
+        document.getElementById('video-list-3'),
+        document.getElementById('video-list-4')
+    ];
+
+    channelIds.forEach((channelId, index) => {
+        const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=50&key=${apiKey}&type=video`;
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                data.items.forEach(video => {
+                    const videoElement = document.createElement('div');
+                    videoElement.innerHTML = `
+                        <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
+                            <img src="${video.snippet.thumbnails.medium.url}" alt="${video.snippet.title}">
+                        </a>
+                        <h3>${video.snippet.title}</h3>
+                    `;
+                    videoColumns[index].appendChild(videoElement);
+                });
+            })
+            .catch(error => console.error('Error fetching video data:', error));
+    });
+});
